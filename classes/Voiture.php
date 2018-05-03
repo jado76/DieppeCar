4bc04a3424ed9149a4d5561e0f79d0599ae70919
jado <tony76.200@hotmail.fr>
2018-05-03 11:42:44 +0200

    classe abstrait voiture

---
+++ classes/Voiture.php
@@ -0,0 +1,21 @@
+<?php
+
+
+abstract class Voiture
+{
+    protected $couleurDominante;
+    protected $masse;
+    protected $marque;
+    protected $modele;
+    protected $vitesse;
+
+    public function __construct($mark,$model)
+    {
+        $this->marque = $mark;
+        $this->modele = $model;
+    }
+
+    abstract public function distanceFreinage();
+
+
+}
